8ea343c1b0b226980c13982a21110f4357a98f72
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-09-05 20:38:09 +0300

    Error handling

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,23 @@ import express from 'express';
 
 import movieRoutes from './app/routes/movie.routes';
 
+const urlPrefix = '/api';
 const app = express();
 
-app.use(movieRoutes.path, movieRoutes.router);
+app.use(urlPrefix + movieRoutes.path, movieRoutes.router);
+
+app.use((err: any, _req: express.Request, _res: express.Response, next: express.NextFunction) => {
+    console.error(err.stack);
+    next(err);
+});
+
+app.use((err: any, _req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(500).json({ error: err.errors || err.message || 'Unknown Error' });
+});
+
+app.use((_req: express.Request, res: express.Response) => {
+    res.status(404).json({ error: 'Not found' });
+});
 
 app.listen(3000, () => {
     console.log('The application is started');
