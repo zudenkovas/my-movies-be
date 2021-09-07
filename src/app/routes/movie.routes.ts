6095c09791562daef559bed384ebdd5c63632696
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-09-07 21:46:09 +0300

    Try pipeline

--- src/app/routes/movie.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { getMovies } from '../controllers/movie.controller';
-
-const router = express.Router();
-
-router.route("/").get(getMovies);
-
-export default { path: '/movies', router };
