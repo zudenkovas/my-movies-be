11670cedee29086a56b483d1d597dcba7d653033
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-09-22 23:00:21 +0300

    Movie service

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,6 +1,8 @@
-const loadMovies = (): Movie[] => {
-  //  Load movies from URL: https://api.themoviedb.org/3/trending/movie/week?sort_by=popularity.desc&api_key=${process.env.API_KEY}
-  return []; // Return loaded movies as a result
+const loadMovies = async (): Promise<Movie[]> => {
+  // Load movies from URL: `https://api.themoviedb.org/3/trending/movie/week?sort_by=popularity.desc&api_key=${process.env.API_KEY}`
+  // use: await axios.get<Response>(url)
+  // convert ResponseMovie objects to Movie objects and return as result
+  return [];
 };
 
 export { loadMovies };
