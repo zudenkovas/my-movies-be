fab2937acc9c4e56616e7680a5699f521641917d
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-09-22 23:05:32 +0300

    Movie controller

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
-const getMovies = (_req: express.Request, res: express.Response): void => {
-  res.json([]); // Change empty array into loadMovies function result in movie.service.ts
+const getMovies = async (_req: express.Request, res: express.Response): Promise<void> => {
+  res.json([]); // Remove empty array with movie.service.ts loadMovies function
 };
 
 export { getMovies };
