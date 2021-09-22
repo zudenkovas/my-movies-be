eee95df6b186e983c10b288c255ba513b03636c8
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-09-22 22:48:44 +0300

    Domain update and acios types

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,4 +1,8 @@
-interface Movie {
+interface Response {
+  results: ResponseMovie[];
+}
+
+interface ResponseMovie {
   id: number;
   title: string;
   release_date: string;
@@ -10,5 +14,20 @@ interface Movie {
   poster_path: string;
   vote_average: number;
   vote_count: number;
+  popularity: number;
+}
+
+interface Movie {
+  id: number;
+  title: string;
+  releaseDate: string;
+  originalLanguage: string;
+  overview: string;
+  genreIds: number[];
+  adult: boolean;
+  backdropPath: string;
+  posterPath: string;
+  voteAverage: number;
+  voteCount: number;
   popularity: number;
 }
