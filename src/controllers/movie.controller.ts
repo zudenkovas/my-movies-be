d32a50ac53e29dccf38d902a91e2171a2475de50
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-09-22 23:28:07 +0300

    Complete implementation

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -3,8 +3,8 @@ import express from 'express';
 import { loadMovies } from '../services/movie.service';
 
 const getMovies = async (_req: express.Request, res: express.Response): Promise<void> => {
-  await loadMovies(); // Store loaded movies to the const
-  res.json([]); // Remove empty array with loaded movies
+  const movies: Movie[] = await loadMovies();
+  res.json(movies);
 };
 
 export { getMovies };
--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,16 +1,28 @@
 import axios from 'axios';
 
-// Uncomment function and complete implementation. Function transforms ResponseMovie to Movie object
-// const convertToMovie = (responseMovie: ResponseMovie): Movie => {
-//   return {};
-// };
+const convertToMovie = (responseMovie: ResponseMovie): Movie => {
+  return {
+    id: responseMovie.id,
+    title: responseMovie.title,
+    releaseDate: responseMovie.release_date,
+    originalLanguage: responseMovie.original_language,
+    overview: responseMovie.overview,
+    genreIds: responseMovie.genre_ids,
+    adult: responseMovie.adult,
+    backdropPath: responseMovie.backdrop_path,
+    posterPath: responseMovie.poster_path,
+    voteAverage: responseMovie.vote_average,
+    voteCount: responseMovie.vote_count,
+    popularity: responseMovie.popularity,
+  };
+};
 
 const loadMovies = async (): Promise<Movie[]> => {
-  await axios.get<Response>( // Store axios.get result into: const { data } =
+  const { data } = await axios.get<Response>(
     `https://api.themoviedb.org/3/trending/movie/week?sort_by=popularity.desc&api_key=${process.env.API_KEY}`,
   );
 
-  return []; // Remove empty array with: data.results.map(convertToMovie);
+  return data.results.map(convertToMovie);
 };
 
 export { loadMovies };
