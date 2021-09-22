134a59ad5c9991ed991964ca36f5879029910853
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-09-22 23:20:04 +0300

    Easy implementation

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
+import { loadMovies } from '../services/movie.service';
+
 const getMovies = async (_req: express.Request, res: express.Response): Promise<void> => {
-  res.json([]); // Remove empty array with movie.service.ts loadMovies function
+  await loadMovies(); // Store loaded movies to the const
+  res.json([]); // Remove empty array with loaded movies
 };
 
 export { getMovies };
--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,8 +1,16 @@
+import axios from 'axios';
+
+// Uncomment function and complete implementation. Function transforms ResponseMovie to Movie object
+// const convertToMovie = (responseMovie: ResponseMovie): Movie => {
+//   return {};
+// };
+
 const loadMovies = async (): Promise<Movie[]> => {
-  // Load movies from URL: `https://api.themoviedb.org/3/trending/movie/week?sort_by=popularity.desc&api_key=${process.env.API_KEY}`
-  // use: await axios.get<Response>(url)
-  // convert ResponseMovie objects to Movie objects and return as result
-  return [];
+  await axios.get<Response>( // Store axios.get result into: const { data } =
+    `https://api.themoviedb.org/3/trending/movie/week?sort_by=popularity.desc&api_key=${process.env.API_KEY}`,
+  );
+
+  return []; // Remove empty array with: data.results.map(convertToMovie);
 };
 
 export { loadMovies };
