c4c2408a81c1e90194f1bd9466570b93be64f56e
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-10-12 17:16:33 +0300

    Health check

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const getHealth = (_req: express.Request, res: express.Response): void => {
   res.json({
-    healthy: true,
+    health: true,
+    version: 'v0.4',
+    mongo: mongoose.connection.readyState > 0,
   });
 };
 
