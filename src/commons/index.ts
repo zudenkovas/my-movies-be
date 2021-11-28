c6446aaa68043ea0dc564f256cf53f4f633c798c
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-11-28 18:35:07 +0200

    Allow all methods

--- src/commons/index.ts
+++ src/commons/index.ts
@@ -5,8 +5,9 @@ import mongoose from 'mongoose';
 export const PAGE_SIZE = 20;
 
 export const CORS = (_req: express.Request, res: express.Response, next: express.NextFunction): void => {
-  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
   next();
 };
 
